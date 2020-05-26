ffb47a67aae59db28c420a50146bc32dabf9563b
guolingege <guolingege@qq.com>
2020-05-26 10:52:13 +0800

    1.4

---
+++ BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  LosslessJSON
+//
+//  Created by mac on 2020/5/26.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+
+#endif /* BridgeHeader_h */
